fbe2f1414ab04eb252058b12a51df08fb3bafc9e
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-12 20:55:56 +0530

    Basic UICollection View Setup

---
+++ CoderSwag/View/ProductsCVCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductsCVCell.swift
+//  CoderSwag
+//
+//  Created by Ravi Inder Manshahia on 12/02/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class ProductsCVCell: UICollectionViewCell {
+    @IBOutlet weak var productImage : UIImageView!
+    @IBOutlet weak var productName : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
+    
+}
