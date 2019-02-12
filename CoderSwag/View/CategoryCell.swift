4fd0e8584260de350511270c70e10b7bced1e72e
Ravi Inder Manshahia <ravinder@lab91.co>
2019-02-12 12:34:44 +0530

    Basic setup Tableview

---
+++ CoderSwag/View/CategoryCell.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryCell.swift
+//  CoderSwag
+//
+//  Created by Ravi Inder Manshahia on 12/02/19.
+//  Copyright © 2019 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage : UIImageView!
+    @IBOutlet weak var categoryLabel : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
